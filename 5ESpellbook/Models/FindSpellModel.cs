e6cc3161a96af793e6f88bed790a123b26888041
reiphil <reiphil@gmail.com>
2015-12-17 13:28:16 -0800

    Initial Files.

---
+++ 5ESpellbook/Models/FindSpellModel.cs
@@ -0,0 +1,19 @@
+﻿using _5ESpellbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5ESpellbook.Models
+{
+    public class FindSpellModel
+    {
+        public Spell spell { get; set; }
+        public List<string> suggestions { get; set; }
+
+        public FindSpellModel()
+        {
+            suggestions = new List<string>();
+        }
+    }
+}
---
+++ 5ESpellbook/Models/SpellLinkModel.cs
@@ -0,0 +1,18 @@
+﻿using _5ESpellbook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5ESpellbook.Models
+{
+    public class SpellLinkModel
+    {
+        public List<Spell> spells { get; set; }
+
+        public SpellLinkModel()
+        {
+            spells = new List<Spell>();
+        }
+    }
+}
